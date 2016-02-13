990525b64e7173e79da41cf611657813ca046b6c
Cezar Vaz <cezarvaz@gmail.com>
2016-02-13 18:19:48 -0200

    Bug fix

--- spec/features/user_visualizes_recipes_spec.rb
+++ spec/features/user_visualizes_recipes_spec.rb
@@ -6,6 +6,11 @@ feature 'Visualize a new recipe' do
 
     visit root_path
 
+    expect(page).to have_content('Tipo')
+    expect(page).to have_content('Preferência')
+    expect(page).to have_content('Cozinha')
+    expect(page).to have_content('Últimas Receitas')
+    expect(page).to have_content('Favoritas dos Usuários')
     expect(page).to have_content(recipe.name)
     expect(page).to have_content(recipe.cuisine)
     expect(page).to have_content(recipe.type_of_food)
