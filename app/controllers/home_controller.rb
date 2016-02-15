61834c1f164d618455787d0cb25adb3e06b587f5
Cezar Vaz <cezarvaz@gmail.com>
2016-02-15 17:05:59 -0200

    Trying to work this out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(created_at: :desc).limit(5)
     @cuisines = Cuisine.all
     @types = Type.all
     @preferences = Preference.all
